2317cf352f01ef61ae664a6ae33fcbf41c4b4bef
tutt1999nd1 <thanhtu199969@gmail.com>
2022-07-29 14:27:32 +0700

    toast login form

--- lib/authentication/signin.dart
+++ lib/authentication/signin.dart
@@ -6,11 +6,31 @@ import 'package:flutter/material.dart';
 import '../home/dashboard.dart';
 import 'constants/constants.dart';
 
-class SignInScreen extends StatelessWidget {
-  const SignInScreen({
-    Key? key,
-  }) : super(key: key);
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({Key? key}) : super(key: key);
 
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController email = new TextEditingController();
+  TextEditingController password = new TextEditingController();
+  bool signIn(){
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(email.text),
+    ));
+    if(email.text=='admin' && password.text =='123456'){
+      print(email.text);
+      print(password.text);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(email.text),
+      ));
+      return true;
+    }
+    else return false;
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +59,7 @@ class SignInScreen extends StatelessWidget {
                         padding: const EdgeInsets.only(bottom: 5),
                         child: Align(
                           alignment: Alignment.centerLeft,
-                          child: Text('Email',
+                          child: Text('Username',
                               style: TextStyle(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 16,
@@ -52,20 +72,21 @@ class SignInScreen extends StatelessWidget {
                         children: [
                           Expanded(
                             child: TextField(
+                              controller: email,
                               decoration: InputDecoration(
                                   prefixIcon:
                                   Icon(Icons.account_box, color: kPrimaryColor),
-                                  hintText: 'Enter the email',
+                                  hintText: 'Enter the username',
                                   fillColor: kPrimaryColor,
                                   border: OutlineInputBorder(),
                                   focusedBorder: OutlineInputBorder(
                                     borderSide:
                                     const BorderSide(color: kPrimaryColor),
                                   ),
-                        enabledBorder: OutlineInputBorder(
-                    borderSide:
-                    const BorderSide(color: kPrimaryColor),
-            )
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide:
+                                    const BorderSide(color: kPrimaryColor),
+                                  )
                               ),
 
                               style: TextStyle(color: kPrimaryColor),
@@ -91,6 +112,7 @@ class SignInScreen extends StatelessWidget {
                         children: [
                           Expanded(
                             child: TextField(
+                              controller: password,
                               obscureText: true,
                               decoration: InputDecoration(
                                   prefixIcon:
@@ -115,7 +137,29 @@ class SignInScreen extends StatelessWidget {
                     Padding(padding: const EdgeInsets.only(bottom: 5), child:
                     ElevatedButton(
                       onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              // Retrieve the text the that user has entered by using the
+                              // TextEditingController.
+                              content: Text(email.text),
+                            );
+                          },
+                        );
                         // Respond to button press
+                        if(signIn == true){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: ((context) => DashBoard())));
+                        }
+                        else {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("Login fail"),
+                          ));
+                        }
+
+
                       },
                       child: Text('Login'),
                       // style: ButtonStyle(
@@ -137,5 +181,6 @@ class SignInScreen extends StatelessWidget {
       ),
       // body:
     );
+
   }
 }
